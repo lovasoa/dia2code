721a78cd89256ceada83fa060744e4dfecdc9ce8
toor <toor>
2002-12-22 10:38:21 +0000

    okellogg:  Generate "namespace" for packages.  Use function print() for proper indentation.  Make variables as local as possible.

--- dia2code/dia2code/generate_code_csharp.c
+++ dia2code/dia2code/generate_code_csharp.c
@@ -21,36 +21,45 @@
 
 #include "dia2code.h"
 
-void generate_code_csharp(batch *b) {
-    umlclasslist tmplist, parents;
-    umlassoclist associations;
-    umlattrlist umla, tmpa;
-    umlpackagelist tmppcklist;
-    umloplist umlo;
-    char *tmpname;
-    char outfilename[90];
-    FILE *licensefile = NULL;
-    umlclasslist used_classes;
+static char *
+pkgname (umlpackagelist pkg)
+{
+    static char buf[512];
 
-    tmplist = b->classlist;
+    buf[0] = '\0';
+    while (pkg != NULL) {
+        strcat (buf, pkg->key->name);
+        pkg = pkg->next;
+        if (pkg != NULL)
+            strcat (buf, ".");
+    }
+    return buf;
+}
+
+void generate_code_csharp (batch *b) {
+    umlclasslist tmplist = b->classlist;
+    FILE *licensefile = NULL;
 
     /* open license file */
-    if ( b->license != NULL ) {
-        licensefile = fopen(b->license, "r"); 
-        if(!licensefile) {
-            fprintf(stderr, "Can't open the license file.\n");
-            exit(2);
+    if (b->license != NULL) {
+        licensefile = fopen (b->license, "r"); 
+        if (!licensefile) {
+            fprintf (stderr, "Can't open the license file.\n");
+            exit (1);
         }
     }
 
-    while ( tmplist != NULL ) {
+    while (tmplist != NULL) {
+        char *tmpname;
+        char outfilename[90];
+        umlclasslist used_classes;
 
         if (is_present (b->classes, tmplist->key->name) ^ b->mask) {
             tmplist = tmplist->next;
             continue;
         }
 
-        sprintf(outfilename, "%s.cs", tmplist->key->name);
+        sprintf (outfilename, "%s.cs", tmplist->key->name);
 
         spec = open_outfile (outfilename, b);
         if (spec == NULL) {
@@ -65,49 +74,38 @@ void generate_code_csharp(batch *b) {
             while ((lc = fgetc (licensefile)) != EOF)
                 print ("%c", lc);
         }
-        emit ("%s","using System;\n");
-
-        tmppcklist = make_package_list(tmplist->key->package);
-        if ( tmppcklist != NULL ){
-            emit ("package %s",tmppcklist->key->name);
-            tmppcklist=tmppcklist->next;
-            while (tmppcklist != NULL) {
-                emit (".%s",tmppcklist->key->name);
-                tmppcklist=tmppcklist->next;
-            }
-            emit (";\n\n");
-        }
+        emit ("%s", "using System;\n\n");
 
         /* We generate the import clauses */
-        used_classes = list_classes(tmplist, b);
-        while (used_classes != NULL) {
-            tmppcklist = make_package_list(used_classes->key->package);
-            if (tmppcklist != NULL) {
-                if (strcmp(tmppcklist->key->id,tmplist->key->package->id)) {
-                    /* This class' package and our current class' package are
-                       not the same */
-                    emit ("using ");
-                    emit ("%s",tmppcklist->key->name);
-                    tmppcklist=tmppcklist->next;
-                    while (tmppcklist != NULL) {
-                        emit (".%s", tmppcklist->key->name);
-                        tmppcklist=tmppcklist->next;
+        used_classes = list_classes (tmplist, b);
+        if (used_classes != NULL) {
+            while (used_classes != NULL) {
+                umlpackagelist pkg = make_package_list (used_classes->key->package);
+                if (pkg != NULL) {
+                    if (strcmp (pkg->key->id, tmplist->key->package->id)) {
+                        /* This class' package and our current class' package are
+                           not the same */
+                        emit ("using %s.%s;\n", pkgname (pkg), used_classes->key->name);
                     }
-                    emit (".");
-                    emit ("%s;\n",used_classes->key->name);
+                } else {
+                    /* No info for this class' package, we include it directly */
+                    /*emit ("import %s;\n",used_classes->key->name);*/
                 }
-            } else {
-                /* No info for this class' package, we include it directly */
-                /*emit ("import %s;\n",used_classes->key->name);*/
+                used_classes = used_classes->next;
             }
-            used_classes = used_classes->next;
+            emit ("\n");
         }
 
-        emit ("\n");
-        emit ("public ");
+        if (tmplist->key->package != NULL) {
+            umlpackagelist pkg = make_package_list (tmplist->key->package);
+            emit ("namespace %s {\n\n", pkgname (pkg));
+            indentlevel++;
+        }
 
-        tmpname = strtolower(tmplist->key->stereotype);
-        if ( ! strcmp("interface", tmpname) ) {
+        print ("public ");
+
+        tmpname = strtolower (tmplist->key->stereotype);
+        if (! strcmp ("interface", tmpname)) {
             emit ("interface ");
         } else {
             if (tmplist->key->isabstract)
@@ -117,138 +115,156 @@ void generate_code_csharp(batch *b) {
             else
                 emit ("class ");
         }
-        free(tmpname);
+        free (tmpname);
 
         emit ("%s", tmplist->key->name);
 
-        parents = tmplist->parents;
-        if (parents != NULL) {
+        if (tmplist->parents != NULL) {
+            umlclasslist parents = tmplist->parents;
             while (parents != NULL) {
-                tmpname = strtolower(parents->key->stereotype);
+                tmpname = strtolower (parents->key->stereotype);
                 if (! strcmp (tmpname, "interface")) {
                     emit (" : ");
                 } else {
                     emit (" : ");
                 }
-                free(tmpname);
+                free (tmpname);
                 emit ("%s", parents->key->name);
                 parents = parents->next;
             }
         }
-        emit (" {\n");
+        emit (" {\n\n");
+        indentlevel++;
 
-        umla = tmplist->key->attributes;
+        if (tmplist->key->attributes != NULL) {
+            umlattrlist umla = tmplist->key->attributes;
 
-        if( umla != NULL)
-            emit ("\n\t// Attributes\n");
+            print ("// Attributes\n");
 
-        while ( umla != NULL) {
-            switch (umla->key.visibility) {
-            case '0':
-                emit ("\tpublic ");
-                break;
-            case '1':
-                emit ("\tprivate ");
-                break;
-            case '2':
-                emit ("\tprotected ");
-                break;
-            }
-
-            if (umla->key.isstatic) {
-                emit ("static ");
-            }
-            emit ("%s %s", umla->key.type, umla->key.name);
-            if (umla->key.value[0] != 0) {
-                emit (" = %s", umla->key.value);
-            }
-            emit (";\n");
-            umla = umla->next;
-        }
-
-        associations = tmplist->associations;
-        if (associations != NULL)
-            emit ("\n\t// Associations \n");
-
-        while (associations != NULL)
-        {
-            /* Not sure how to do this actually...*/
-            if( associations->composite )
-                emit ("\tprotected %s %s;\n", associations->key->name, associations->name);
-            else
-                emit ("\tprivate %s %s;\n", associations->key->name, associations->name);
-            associations = associations->next;
-        }
-
-        umlo = tmplist->key->operations;
-        while (umlo != NULL) {
-            emit ("\n");
-            emit ("\t// Operation\n");
-            tmpa = umlo->key.parameters;
-            while (tmpa != NULL) {
-                emit ("\t// param %s\n", tmpa->key.name);
-                tmpa = tmpa->next;
-            }
-            if (strcmp (umlo->key.attr.type, "void"))
-                emit ("\t// return %s\n", umlo->key.attr.type);
-
-
-            emit ("\t");
-
-            if (umlo->key.attr.isabstract)
-            {
-                /*emit ("public ");*/
-                umlo->key.attr.value[0] = '0';
-            }
-            else
-            {
-                switch (umlo->key.attr.visibility) {
+            while (umla != NULL) {
+                switch (umla->key.visibility) {
                 case '0':
-                    emit ("public ");
+                    print ("public ");
                     break;
                 case '1':
-                    emit ("private ");
+                    print ("private ");
                     break;
                 case '2':
-                    emit ("protected ");
+                    print ("protected ");
                     break;
                 }
-            }
-            if (umlo->key.attr.isstatic) {
-                emit ("static ");
-            }
-            if (strlen (umlo->key.attr.type) > 0) {
-                emit ("%s ", umlo->key.attr.type);
-            }
-            emit ("%s(", umlo->key.attr.name);
-            tmpa = umlo->key.parameters;
-            while (tmpa != NULL) {
-                emit ("%s %s", tmpa->key.type, tmpa->key.name);
-                /*
-                if ( tmpa->key.value[0] != 0 ){
-                    emit (" = %s",tmpa->key.value);
-                }  */
-                tmpa = tmpa->next;
-                if (tmpa != NULL) emit (", ");
-            }
-            emit (")");
-            if (umlo->key.attr.isabstract ) {
-                emit (";\n");
-            } else {
-                emit ("\n\t{\n");
-                if (umlo->key.implementation != NULL) {
-                    emit ("\n\t\t%s\n", umlo->key.implementation);
-                }
-                else if (strcmp(umlo->key.attr.type, "void") != 0) {
-                    emit ("\t\t%s\n", "throw new System.Exception( \"Not implemented yet!\");");
-                }
-                emit ("\t}\n");
-            }
-            umlo = umlo->next;
-        }
-        emit ("}\n\n");
 
-        fclose(spec);
+                if (umla->key.isstatic) {
+                    emit ("static ");
+                }
+                emit ("%s %s", umla->key.type, umla->key.name);
+                if (umla->key.value[0] != 0) {
+                    emit (" = %s", umla->key.value);
+                }
+                emit (";\n");
+                umla = umla->next;
+            }
+            emit ("\n");
+        }
+
+        if (tmplist->associations != NULL) {
+            umlassoclist assoc = tmplist->associations;
+
+            print ("// Associations \n");
+            while (assoc != NULL)
+            {
+                /* Not sure how to do this actually...*/
+                if (assoc->composite )
+                    print ("protected ");
+                else
+                    print ("private ");
+                emit ("%s %s;\n", assoc->key->name, assoc->name);
+                assoc = assoc->next;
+            }
+            emit ("\n");
+        }
+
+        if (tmplist->key->operations != NULL) {
+            umloplist umlo = tmplist->key->operations;
+            while (umlo != NULL) {
+                umlattrlist tmpa = umlo->key.parameters;
+
+                print ("// Operation\n");
+                while (tmpa != NULL) {
+                    print ("// param %s\n", tmpa->key.name);
+                    tmpa = tmpa->next;
+                }
+                if (strcmp (umlo->key.attr.type, "void"))
+                    print ("// return %s\n", umlo->key.attr.type);
+
+
+                print ("");
+
+                if (umlo->key.attr.isabstract)
+                {
+                    /*emit ("public ");*/
+                    umlo->key.attr.value[0] = '0';
+                }
+                else
+                {
+                    switch (umlo->key.attr.visibility) {
+                    case '0':
+                        emit ("public ");
+                        break;
+                    case '1':
+                        emit ("private ");
+                        break;
+                    case '2':
+                        emit ("protected ");
+                        break;
+                    }
+                }
+                if (umlo->key.attr.isstatic) {
+                    emit ("static ");
+                }
+                if (strlen (umlo->key.attr.type) > 0) {
+                    emit ("%s ", umlo->key.attr.type);
+                }
+                emit ("%s (", umlo->key.attr.name);
+                tmpa = umlo->key.parameters;
+                while (tmpa != NULL) {
+                    emit ("%s %s", tmpa->key.type, tmpa->key.name);
+                    /*
+                    if ( tmpa->key.value[0] != 0 ){
+                        emit (" = %s",tmpa->key.value);
+                    }  */
+                    tmpa = tmpa->next;
+                    if (tmpa != NULL)
+                        emit (", ");
+                }
+                emit (")");
+                if (umlo->key.attr.isabstract ) {
+                    emit (";\n");
+                } else {
+                    emit ("\n");
+                    print ("{\n");
+                    indentlevel++;
+                    if (umlo->key.implementation != NULL) {
+                        print ("%s\n", umlo->key.implementation);
+                    } else if (strcmp (umlo->key.attr.type, "void") != 0) {
+                        print ("%s\n", "throw new System.Exception (\"Not implemented yet!\");");
+                    }
+                    indentlevel--;
+                    print ("}\n");
+                }
+                umlo = umlo->next;
+            }
+            emit ("\n");
+        }
+        indentlevel--;
+        print ("}\n\n");
+
+        if (tmplist->key->package != NULL) {
+            indentlevel--;
+            print ("}\n\n");
+        }
+
+        fclose (spec);
         tmplist = tmplist->next;
     }
 }
